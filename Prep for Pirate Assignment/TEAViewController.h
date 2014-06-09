04f3e339d7181f3a8b62d0a4f6502ba59dad6850
Tony Angelo <tonyangelox@veronica.home>
2014-06-08 20:28:44 -0400

    Initial Commit

---
+++ Prep for Pirate Assignment/TEAViewController.h
@@ -0,0 +1,13 @@
+//
+//  TEAViewController.h
+//  Prep for Pirate Assignment
+//
+//  Created by Tony Angelo on 6/8/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TEAViewController : UIViewController
+
+@end
---
+++ Prep for Pirate Assignment/TEAViewController.m
@@ -0,0 +1,29 @@
+//
+//  TEAViewController.m
+//  Prep for Pirate Assignment
+//
+//  Created by Tony Angelo on 6/8/14.
+//  Copyright (c) 2014 Tony Angelo. All rights reserved.
+//
+
+#import "TEAViewController.h"
+
+@interface TEAViewController ()
+
+@end
+
+@implementation TEAViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
